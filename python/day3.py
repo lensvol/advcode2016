37b1e7396e1a91271fedae5abfaec6e0aa3cfd6f
Kirill Borisov <lensvol@gmail.com>
2016-12-06 13:39:41 +0300

    Part B of Day 3 done.

--- python/day3.py
+++ python/day3.py
@@ -4,6 +4,7 @@
 Day 3 of the Advent of Code challenge, parts A and B
 '''
 
+from itertools import chain, islice, ifilter, tee
 import sys
 import re
 
@@ -20,6 +21,10 @@ def parse_sides(text):
 
 
 def is_triangle_possible(given_sides):
+    '''
+    Check to see if triangle is possible one
+    (sum of any two sides should be greater than a remaining side)
+    '''
     return all([
         given_sides[0] + given_sides[1] > given_sides[2],
         given_sides[0] + given_sides[2] > given_sides[1],
@@ -27,11 +32,40 @@ def is_triangle_possible(given_sides):
     ])
 
 
+def grouper(iterable, chunk_size):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(islice(it, chunk_size))
+        if not chunk:
+            return
+        yield chunk
+
+
+def extract_triangles_from_columns(sides):
+    '''
+    Transform incoming list of number tuples into
+    list of numbers from columns.
+    '''
+    row_stream = chain(*sides)
+    splits = tee(row_stream, 3)
+
+    columns = chain(*[
+        islice(split, start, None, 3)
+        for start, split in enumerate(splits)
+    ])
+
+    return grouper(columns, 3)
+
+
 if __name__ == '__main__':
     with open(sys.argv[1]) as fp:
         triangles = fp.readlines()
         triangles = map(lambda l: l.strip(), triangles)
 
-        regular_sides = map(parse_sides, triangles)
-        possible_regular_triangles = filter(is_triangle_possible, regular_sides)
+        sides = map(parse_sides, triangles)
+
+        possible_regular_triangles = filter(is_triangle_possible, sides)
+        possible_column_triangles = filter(is_triangle_possible, extract_triangles_from_columns(sides))
+
         print 'Possible conventional triangles:', len(possible_regular_triangles)
+        print 'Possible triangles from columns:', len(possible_column_triangles)
